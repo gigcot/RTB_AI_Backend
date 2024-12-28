fac27439923a6a23bfaef347ed708579f7c6a9cd
gigcot <gigcot@gmail.com>
2024-12-28 14:43:17 +0900

    feat: 응답 품질 개선을 위한 역할 이름과 시스템 메시지 설정

--- utils/common/base_phase_nodes.py
+++ utils/common/base_phase_nodes.py
@@ -7,6 +7,7 @@ class BasePhaseNodes:
         self.phase_manager = PhaseStateManager()  
         self.llm_engine = llm_engine
         self.logger = Logger(f"{self.__class__.__name__.replace('PhaseNodes','')}").get_logger()
+        self.si_follow_prompt = "당신은 SI-Follow의 AI 조직원입니다. SI-Follow는 고객의 요구에 맞춘 신뢰성 있는 맞춤형 소프트웨어 솔루션을 제공하는 AI IT파트너입니다.조직원 모두 명확한 역할을 맡아 협력하고 있으며, 프로젝트의 성공을 목표로 합니다."
     def phase_prompt_update():
         raise NotImplementedError(" must be implemented in subclasses.")
 
@@ -14,13 +15,13 @@ class BasePhaseNodes:
         self.phase_manager.state = self.env_manager.get_full_state()
         
     def generate_messages(self):
-        user_prompt = self.phase_prompt
-        filled_prompt = user_prompt.format(**self.phase_manager.state)
-        assistant_prompt = self.assistant_prompt
+        user_prompt = self.phase_prompt.format(**self.phase_manager.state)
+        assistant_prompt = self.assistant_role_prompt.format(**self.phase_manager.state)
         return [
-            {"role": "assistant", "content": assistant_prompt},
-            {"role": "user", "content": filled_prompt}
-        ]
+                {"role": "system", "content": self.si_follow_prompt},
+                {"role": "user", "name": "coworker", "content": user_prompt},
+                {"role": "assistant", "name": self.assistant_role_name, "content": assistant_prompt},
+                ]
 
     def call_llm(self, messages):
         response = self.llm_engine.call_openai_api(messages)
